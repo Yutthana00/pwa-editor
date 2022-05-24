60ca6f2f95001777b7880a73c8b42f026cc8a1ae
Yutthana00 <arf101293@gmail.com>
2022-05-25 00:20:20 +0100

    Add logic for method get all content from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -28,6 +28,18 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  // Connect to database and choose version 1.
+  const jateDB = await openDB('jate', 1);
+  // Create new transaction specifying the database and data privilege.
+  const tx = jateDB.transaction(['jade'], 'readonly');
+  // Open the desired object store.
+  const store = tx.objectStore('jate');
+  // Use the put method to edit content.
+  const request = store.getAll();
+  // Get confirmation of request.
+  const result = await request;
+  console.log(result);
+};
 
 initdb();
