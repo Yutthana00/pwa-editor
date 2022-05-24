aba7959be0376cccbff7ba3faa0300f45f096030
Yutthana00 <arf101293@gmail.com>
2022-05-25 00:14:59 +0100

    Add logic to a method and adds it to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,19 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // Connect to database and choose version 1.
+  const jateDB = await openDB('jate', 1);
+  // Create new transaction specifying the database and data privilege.
+  const tx = jateDB.transaction(['jade'], 'readwrite');
+  // Open the desired object store.
+  const store = tx.objectStore('jate');
+  // Use the put method to edit content.
+  const request = store.put({ id:1 , value: content });
+  // Get confirmation of request.
+  const result = await request;
+  console.log('Successfully saved in database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
